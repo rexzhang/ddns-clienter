5f32f562cd5072449b32b40853ef08c60f471697
Rex Zhang <rex.zhang@gmail.com>
2021-11-25 13:57:30 +0800

    Reformat by Black

--- ddns_clienter/asgi.py
+++ ddns_clienter/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ddns_clienter.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ddns_clienter.settings")
 
 application = get_asgi_application()
--- ddns_clienter_core/apps.py
+++ ddns_clienter_core/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class DdnsClienterCoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ddns_clienter_core'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "ddns_clienter_core"
