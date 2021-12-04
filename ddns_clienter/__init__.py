3b9403899adf6d33e03179bdf4b7f5ff9be43bd5
Rex Zhang <rex.zhang@gmail.com>
2021-12-04 14:09:41 +0800

    Bump version to 0.1.1

--- ddns_clienter/__init__.py
+++ ddns_clienter/__init__.py
@@ -11,7 +11,7 @@ from .core import (  # noqa: F401
 )
 
 __name__ = "DDNS Clienter"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __author__ = "Rex Zhang"
 __author_email__ = "rex.zhang@gmail.com"
