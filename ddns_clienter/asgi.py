4a9a0aac471a95bfc5b4af2194a0a9aa05708086
Rex Zhang <rex.zhang@gmail.com>
2023-02-27 01:13:11 +0800

    Fix static file in docker container

--- ddns_clienter/asgi.py
+++ ddns_clienter/asgi.py
@@ -9,8 +9,21 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 
+from asgi_middleware_static_file import ASGIMiddlewareStaticFile
+from django.conf import settings
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ddns_clienter.settings")
 
 application = get_asgi_application()
+application = ASGIMiddlewareStaticFile(
+    application,
+    static_url=settings.STATIC_URL,
+    static_root_paths=[settings.STATIC_ROOT],
+)
+
+SENTRY_DSN = os.getenv("SENTRY_DSN", "")
+if SENTRY_DSN:
+    from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
+
+    application = SentryAsgiMiddleware(application)
