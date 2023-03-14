97852f13aa118ccabfdc586488360c899bbb998f
Rex Zhang <rex.zhang@gmail.com>
2023-03-14 18:42:56 +0800

    Fix bug

--- ddns_clienter_core/management/commands/check_and_update.py
+++ ddns_clienter_core/management/commands/check_and_update.py
@@ -1,3 +1,4 @@
+from asgiref.sync import async_to_sync
 from django.core.management.base import BaseCommand, CommandError
 
 from ddns_clienter_core.runtimes.check_and_update import check_and_update
@@ -11,6 +12,6 @@ class Command(BaseCommand):
         parser.add_argument("--real-update", action="store_true")
 
     def handle(self, *args, **options):
-        check_and_update(
+        async_to_sync(check_and_update)(
             config_file_name=options["config"], real_update=options["real_update"]
         )
