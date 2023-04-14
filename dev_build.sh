d609a647ef4541c3a6d5fa1130b8ca1238312fcb
Rex Zhang <rex.zhang@gmail.com>
2023-04-14 16:48:30 +0800

    Update shell script

--- dev_build.sh
+++ dev_build.sh
@@ -1,15 +1,18 @@
-#!/bin/sh
+#!/bin/zsh
+
+docker image rm cr.h.rexzhang.com/ray1ex/ddns-clienter
+docker build -t cr.h.rexzhang.com/ray1ex/ddns-clienter . --build-arg ENV=rex --file Dockerfile.dev
+
+read -r -s -k '?Press any key to continue, push docker image...'
+docker push cr.h.rexzhang.com/ray1ex/ddns-clienter
+
+read -r -s -k '?Press any key to continue. startup container...'
 
 docker container stop ddns-clienter
 docker container rm ddns-clienter
-docker image rm cr.rexzhang.com:55555/ray1ex/ddns-clienter
-
-docker build -t cr.rexzhang.com:55555/ray1ex/ddns-clienter . --build-arg ENV=rex --file Dockerfile.dev
-#docker push cr.rexzhang.com:55555/ray1ex/ddns-clienter
-
 docker run -dit -p 0.0.0.0:8000:8000 \
   -v /Users/rex/p/ddns-clienter/config.toml:/etc/ddns-clienter.toml:ro \
   --env-file .env \
-  --name ddns-clienter cr.rexzhang.com:55555/ray1ex/ddns-clienter
+  --name ddns-clienter cr.h.rexzhang.com/ray1ex/ddns-clienter
 docker image prune -f
 docker container logs -f ddns-clienter
