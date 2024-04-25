26e3037bf54e6a2e5abe0764001ec31ff93ee829
Rex Zhang <rex.zhang@gmail.com>
2024-04-25 10:58:38 +0800

    Update runserver.py for SSE

--- runserver.py
+++ runserver.py
@@ -1,21 +1,42 @@
 #!/usr/bin/env python
 
+from logging import getLogger
+from os import getenv
 
+import click
 import uvicorn
 
+logger = getLogger(__name__)
 
-def main():
+
+@click.command()
+@click.option(
+    "-H",
+    "--host",
+    default="127.0.0.1",
+    help="Bind socket to this host.  [default: 127.0.0.1]",
+)
+@click.option(
+    "-P", "--port", default=8000, help="Bind socket to this port.  [default: 8000]"
+)
+@click.option("--dev", is_flag=True, help="for dev stage")
+def cli(**cli_kwargs):
     kwargs = {
-        "host": "0.0.0.0",
-        "port": 8000,
         "app": "ddns_clienter.asgi:application",
-        # TODO: ASGI 'lifespan' protocol appears unsupported. Django 3.1.x
-        "lifespan": "off",
+        "lifespan": "off",  # TODO: ASGI 'lifespan' protocol appears unsupported. Django 3.1.x
+        "log_level": "info",
         "access_log": False,
     }
+    kwargs.update(cli_kwargs)
 
-    uvicorn.run(**kwargs)
+    dev = kwargs.pop("dev", False)
+    debug = getenv("DEBUG", False)
+    if dev or debug:
+        print("!!!Enter Development Stage...!!!")
+        kwargs.update({"access_log": True, "reload": True})
+
+    return uvicorn.run(**kwargs)
 
 
 if __name__ == "__main__":
-    main()
+    cli()
