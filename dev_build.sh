74da65f5efa0d354e09ca0b694a724cd34fc0113
Rex Zhang <rex.zhang@gmail.com>
2024-05-03 15:04:21 +0800

    Update build scripts

--- dev_build.sh
+++ dev_build.sh
@@ -1,9 +1,11 @@
 #!/bin/zsh
 
 docker build -t cr.h.rexzhang.com/ray1ex/ddns-clienter . --build-arg ENV=rex --file Dockerfile.dev
+say "build finished"
 
 read -r -s -k '?Press any key to continue, push docker image...'
 docker push cr.h.rexzhang.com/ray1ex/ddns-clienter
+say "push finished"
 
 read -r -s -k '?Press any key to continue. startup container...'
 
--- dev_build_base.sh
+++ dev_build_base.sh
@@ -2,3 +2,4 @@
 
 docker pull python:3.12-alpine
 docker build -t ray1ex/ddns-clienter-base . --build-arg ENV=rex --file Dockerfile.dev.base
+say "build finished"
